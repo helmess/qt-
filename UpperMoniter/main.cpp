af9c32f502f0f116625f6cabd2b1309d7fd69846
杨川力 <yangchuanli000@qq.com>
2022-06-09 14:40:52 +0800

    优化了界面

--- UpperMoniter/main.cpp
+++ UpperMoniter/main.cpp
@@ -1,10 +1,20 @@
 #include "UpperMoniter.h"
 #include <QtWidgets/QApplication>
-
+#include<qfile.h>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+	QFile file("./main.qss");
+	file.open(QIODevice::ReadOnly);
+	if (file.isOpen())
+	{
+		QString strFile = file.readAll();
+		a.setStyleSheet(strFile);
+		file.close();
+	}
     UpperMoniter w;
     w.show();
+
+
     return a.exec();
 }
